84b6847d212f9b48bc8ea2d85705c82e982e8bc8
OJRICCI <oscarriccioj@gmail.com>
2026-01-23 10:36:35 -0600

    feat: pantalla de mantenimiento inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { MaintenanceComponent } from './shared/components/maintenance/maintenance.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MaintenanceComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
