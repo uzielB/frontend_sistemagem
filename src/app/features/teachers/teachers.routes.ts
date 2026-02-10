d38c5f2896a811ffce6dc96d038679f52e143faa
uzielB <utti222033@utvco.edu.mx>
2026-02-10 09:44:42 -0600

    Reorganizar rutas de profesores: actualizar componentes de calificaciones y gestión grupal

--- src/app/features/teachers/teachers.routes.ts
+++ src/app/features/teachers/teachers.routes.ts
@@ -7,18 +7,23 @@ export const TEACHERS_ROUTES: Routes = [
       .then(m => m.TeacherDashboardComponent)
   },
   {
-    path: 'calificaciones',
-    loadComponent: () => import('./components/grade-form/grade-form.component')
-      .then(m => m.GradeFormComponent)
-  },
-  {
     path: 'asistencias',
-    loadComponent: () => import('./components/attendance-table/attendance-table.component')
-      .then(m => m.AttendanceTableComponent)
+    loadComponent: () => import('./pages/attendance/attendance.component')
+      .then(m => m.AttendanceComponent)
   },
   {
     path: 'cambiar-contrasena',
     loadComponent: () => import('./pages/change-password/change-password.component')
       .then(m => m.ChangePasswordComponent)
-  }
+  },
+  {
+    path: 'calificaciones',
+    loadComponent: () => import('./pages/grades/grades.component')
+      .then(m => m.GradesComponent)
+  },
+  {
+    path: 'gestion-grupal',
+    loadComponent: () => import('./pages/group-management/group-management.component')
+      .then(m => m.GroupManagementComponent)
+  },
 ];
